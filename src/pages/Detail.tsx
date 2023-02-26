7fa6998420f3f82fc56d2e59b45c6c76b9bbc72f
Gilang Ramadhan <gilangr1440@gmail.com>
2023-02-26 13:21:00 +0700

    Release 3 migrate to function component

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import DetailCard from "../components/DetailCard";
 import Navbar from "../components/Navbar";
@@ -16,65 +16,123 @@ interface StateType {
   data: MovieType;
 }
 
-export class Detail extends Component<PropsType, StateType> {
-  constructor(props: PropsType) {
-    super(props);
-    this.state = {
-      data: {},
-    };
-  }
+// export class Detail extends Component<PropsType, StateType> {
+//   constructor(props: PropsType) {
+//     super(props);
+//     this.state = {
+//       data: {},
+//     };
+//   }
 
-  async getMovie() {
-    const { id_movie } = this.props.params;
+//   async getMovie() {
+//     const { id_movie } = this.props.params;
+//     axios
+//       .get(`https://api.themoviedb.org/3/movie/${id_movie}?api_key=${import.meta.env.VITE_API_KEY}&language=en-US`)
+//       .then((response) => {
+//         console.log("data: ", response.data);
+//         this.setState({ data: response.data });
+//       })
+//       .catch((error) => {
+//         console.log("error: ", error);
+//       });
+//   }
+
+//   componentDidMount() {
+//     this.getMovie();
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <Navbar />
+//         <div className="flex justify-center">
+//           <div className="card card-side bg-base-100 shadow-xl p-5 w-3/4">
+//             <figure>
+//               <img src={`https://image.tmdb.org/t/p/w500${this.state.data.poster_path}`} width={500} alt={this.state.data.title} />
+//             </figure>
+//             <div className="card-body">
+//               <h2 className="card-title">{this.state.data.title}</h2>
+//               <ul>
+//                 <li>Runtime: {this.state.data.runtime} </li>
+//                 <li>Release date: {this.state.data.release_date} </li>
+//                 <li>
+//                   Genre: {""}{" "}
+//                   {this.state.data.genres
+//                     ?.map((genre) => {
+//                       return genre.name;
+//                     })
+//                     .join(", ")}{" "}
+//                 </li>
+//                 <li>Language: {this.state.data.original_language}</li>
+//                 <li>Overview: {this.state.data.overview}</li>
+//               </ul>
+//               <div className="card-actions justify-end">
+//                 <button className="btn btn-primary">Watch</button>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </>
+//     );
+//   }
+// }
+
+// export default withRouter(Detail);
+
+// import React from "react";
+
+const Detail = () => {
+  const { id_movie } = useParams();
+  const [data, setData] = useState<MovieType>({});
+
+  function getMovie() {
     axios
-      .get(`https://api.themoviedb.org/3/movie/${id_movie}?api_key=${import.meta.env.VITE_API_KEY}&language=en-US`)
-      .then((response) => {
-        console.log("data: ", response.data);
-        this.setState({ data: response.data });
+      .get(`https://api.themoviedb.org/3/movie/${id_movie}?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&append_to_response=videos`)
+      .then((data) => {
+        // const data = data.data;
+        setData(data.data);
       })
       .catch((error) => {
-        console.log("error: ", error);
+        alert(error.toString());
       });
   }
 
-  componentDidMount() {
-    this.getMovie();
-  }
+  useEffect(() => {
+    getMovie();
+  }, []);
 
-  render() {
-    return (
-      <>
-        <Navbar />
-        <div className="flex justify-center">
-          <div className="card card-side bg-base-100 shadow-xl p-5 w-3/4">
-            <figure>
-              <img src={`https://image.tmdb.org/t/p/w500${this.state.data.poster_path}`} width={500} alt={this.state.data.title} />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">{this.state.data.title}</h2>
-              <ul>
-                <li>Runtime: {this.state.data.runtime} </li>
-                <li>Release date: {this.state.data.release_date} </li>
-                <li>
-                  Genre: {""}{" "}
-                  {this.state.data.genres
-                    ?.map((genre) => {
-                      return genre.name;
-                    })
-                    .join(", ")}{" "}
-                </li>
-                <li>Language: {this.state.data.original_language}</li>
-                <li>Overview: {this.state.data.overview}</li>
-              </ul>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Watch</button>
-              </div>
+  return (
+    <>
+      <Navbar />
+      <div className="flex justify-center">
+        <div className="card card-side bg-base-100 shadow-xl p-5 w-3/4">
+          <figure>
+            <img src={`https://image.tmdb.org/t/p/w500${data.poster_path}`} width={500} alt={data.title} />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{data.title}</h2>
+            <ul>
+              <li>Runtime: {data.runtime} </li>
+              <li>Release date: {data.release_date} </li>
+              <li>
+                Genre:{" "}
+                {data.genres
+                  ?.map((genre) => {
+                    return genre.name;
+                  })
+                  .join(", ")}
+              </li>
+              <li>Language: {data.original_language}</li>
+              <li>Overview: {data.overview}</li>
+            </ul>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
             </div>
           </div>
         </div>
-      </>
-    );
-  }
-}
+      </div>
+    </>
+  );
+};
 
-export default withRouter(Detail);
+export default Detail;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,76 +1,139 @@
 import axios from "axios";
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import Card from "../components/Card";
 import Navbar from "../components/Navbar";
 
 import movies from "../dummy/movies.json";
 
-interface MoviesState {
-  data?: [];
-  page: number;
-  total_pages: number;
-}
+// interface MoviesState {
+//   data?: [];
+//   page: number;
+//   total_pages: number;
+// }
 
-export class Homepage extends Component<MoviesState> {
-  state = {
-    data: [],
-    page: 1,
-    total_pages: 0,
-  };
+// export class Homepage extends Component<MoviesState> {
+//   state = {
+//     data: [],
+//     page: 1,
+//     total_pages: 0,
+//   };
 
-  async getMovies(page: number) {
+//   async getMovies(page: number) {
+//     axios
+//       .get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&page=${page}`)
+//       .then((response) => {
+//         console.log("data: ", response.data.results);
+//         this.setState({ data: response.data.results });
+//         this.setState({ total_pages: response.data.total_pages });
+//       })
+//       .catch((error) => {
+//         console.log("error: ", error);
+//       });
+//   }
+
+//   componentDidMount() {
+//     this.getMovies(1);
+//   }
+
+//   nextPage() {
+//     this.setState({ page: this.state.page + 1 });
+//     this.getMovies(this.state.page);
+//   }
+
+//   previousPage() {
+//     this.setState({ page: this.state.page - 1 });
+//     this.getMovies(this.state.page);
+//   }
+
+//   render() {
+//     const { data } = this.state;
+
+//     return (
+//       <div>
+//         <>
+//           <Navbar />
+//           <h1 className="text-center text-3xl py-10">Now Playing Movies</h1>
+//           <div className="grid grid-cols-4 gap-3 p-3">
+//             {data.map((item: any, index) => {
+//               return <Card key={index} id={item.id} title={item.original_title} image={item.poster_path} />;
+//             })}
+//           </div>
+//           <div className="btn-group flex justify-center py-5">
+//             <button disabled={this.state.page <= 1} className="btn" onClick={() => this.previousPage()}>
+//               «
+//             </button>
+//             <button className="btn">Page {this.state.page}</button>
+//             <button disabled={this.state.page === this.state.total_pages} className="btn" onClick={() => this.nextPage()}>
+//               »
+//             </button>
+//           </div>
+//         </>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Homepage;
+
+// import React from "react";
+import { MovieType } from "../utils/types/movie";
+
+const Homepage = () => {
+  const [datas, setDatas] = useState<MovieType[]>([]);
+  const [totalPage, setTotalPage] = useState<number>(1);
+  const [page, setPage] = useState<number>(1);
+
+  function getMovies(page: number) {
     axios
       .get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&page=${page}`)
-      .then((response) => {
-        console.log("data: ", response.data.results);
-        this.setState({ data: response.data.results });
-        this.setState({ total_pages: response.data.total_pages });
+      .then((data) => {
+        const { results, total_pages } = data.data; // destructuring
+        setDatas(results);
+        setTotalPage(total_pages);
       })
       .catch((error) => {
-        console.log("error: ", error);
+        alert(error.toString());
       });
   }
 
-  componentDidMount() {
-    this.getMovies(1);
+  useEffect(() => {
+    getMovies(1);
+  }, []);
+
+  function nextPage() {
+    const newPage = page + 1;
+    setPage(newPage);
+    getMovies(newPage);
   }
 
-  nextPage() {
-    this.setState({ page: this.state.page + 1 });
-    this.getMovies(this.state.page);
+  function previousPage() {
+    const newPage = page - 1;
+    setPage(newPage);
+    getMovies(newPage);
   }
 
-  previousPage() {
-    this.setState({ page: this.state.page - 1 });
-    this.getMovies(this.state.page);
-  }
-
-  render() {
-    const { data } = this.state;
-
-    return (
-      <div>
-        <>
-          <Navbar />
-          <h1 className="text-center text-3xl py-10">Now Playing Movies</h1>
-          <div className="grid grid-cols-4 gap-3 p-3">
-            {data.map((item: any, index) => {
-              return <Card key={index} id={item.id} title={item.original_title} image={item.poster_path} />;
-            })}
-          </div>
-          <div className="btn-group flex justify-center py-5">
-            <button disabled={this.state.page <= 1} className="btn" onClick={() => this.previousPage()}>
-              «
-            </button>
-            <button className="btn">Page {this.state.page}</button>
-            <button disabled={this.state.page === this.state.total_pages} className="btn" onClick={() => this.nextPage()}>
-              »
-            </button>
-          </div>
-        </>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <>
+        <Navbar />
+        <h1 className="text-center text-3xl py-10">Now Playing Movies</h1>
+        <div className="grid grid-cols-4 gap-3 p-3">
+          {datas.map((item: any, index) => {
+            return <Card key={index} id={item.id} title={item.original_title} image={item.poster_path} />;
+          })}
+        </div>
+        <div className="btn-group flex justify-center py-5">
+          <button disabled={page <= 1} className="btn" onClick={() => previousPage()}>
+            «
+          </button>
+          <button className="btn">Page {page}</button>
+          <button disabled={page === totalPage} className="btn" onClick={() => nextPage()}>
+            »
+          </button>
+        </div>
+      </>
+    </div>
+  );
+};
 
 export default Homepage;
